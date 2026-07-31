b30fffc8dff01d82fb017fbbbca27aa5f2a91ef8
Ilya Buzunov <ilya.buzunov@mail.ru>
2026-07-31 13:01:08 +0300

    новая крутая иконка и отображение ее в андроиде

--- src/ChessAnalyzer.Maui/Platforms/Android/MainActivity.cs
+++ src/ChessAnalyzer.Maui/Platforms/Android/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.OS;
 namespace ChessAnalyzer.Maui;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+    Icon = "@mipmap/appicon", RoundIcon = "@mipmap/appicon_round",
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode)]
 public class MainActivity : MauiAppCompatActivity
 {
